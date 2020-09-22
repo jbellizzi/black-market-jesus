64f45d0359ee9a57d51f170fba2d0a208f85a286
John Bellizzi <jbellizzi@axisgroup.com>
2020-09-22 16:57:21 -0400

    adding about page

---
+++ src/routes/About/About.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+export default () => {
+	return (
+		<div className="about">
+			<p>
+				This website was created by John Bellizzi, Solutions Architect at Axis Group, LLC in conjunction with Kelsey
+				Champagne's doctoral thesis,{" "}
+				<span className="title">
+					Migration, Exile and Absence: Catholicism on the British Atlantic Frontier, 1634-1699 (Yale, 2021)
+				</span>
+				.
+			</p>
+			<p>
+				Drawing on over three thousand letters from the Blairs Letters and Scots Missions collections of the Scottish
+				Catholic Archives at the University of Aberdeen, the maps on this interactive site track the movement of letters
+				and individuals connected to Scottish Catholicism between 1640 and 1699. Facing persecution for their faith in
+				the British Isles, many Scots Catholics chose temporary or permanent exile in Catholic Europe where they could
+				openly practice their faith. Many of the men featured in the SCA collections became ordained secular priests and
+				returned to Scotland as missionaries, where they provided catechism to the country’s practicing Catholics and
+				converted more. Their mission flowed from institutions in Europe that trained priests and equipped them with the
+				resources necessary to embark on missionary work back to Scotland. The maps on this site show the mechanisms of
+				the networks they created in the tumultuous period of the seventeenth century.
+			</p>
+			<p>
+				Please enjoy moving through this digital visualization of Scottish Catholicism in exile. For further
+				information, please contact <a href="mailto:kelsey.champagne@yale.edu">Kelsey Champagne</a>.
+			</p>
+		</div>
+	)
+}
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,10 +2,12 @@ import React, { useState } from "react"
 import { Route, Link, withRouter } from "react-router-dom"
 import People from "./People/People"
 import Letters from "./Letters/Letters"
+import About from "./About/About"
 import { withStyles, AppBar, Tabs, Tab } from "@material-ui/core"
 
 const styles = theme => {
-	const tabColor = theme.palette.primary[900]
+	// const tabColor = theme.palette.primary[900]
+	const tabColor = "#3c5f7b"
 
 	return {
 		Tabs: {
@@ -35,15 +37,18 @@ const AppRouter = props => {
 					className={classes.Tabs}
 					value={page}
 					variant="fullWidth"
+					indicatorColor="primary"
 					onChange={(change, value) => setPage(value)}
 				>
 					<Tab label="People" component={Link} to="/" />
 					<Tab label="Letters" component={Link} to="/letters" />
+					<Tab label="About" component={Link} to="/about" />
 				</Tabs>
 			</AppBar>
 
 			<Route path="/" exact component={People} />
 			<Route path="/letters" component={Letters} />
+			<Route path="/about" component={About} />
 		</div>
 	)
 }

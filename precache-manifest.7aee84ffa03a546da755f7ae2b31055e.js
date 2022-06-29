a81ffef0a1a2b5ae648fbbda599e98daadaa3c98
John Bellizzi <jabellizzi406@gmail.com>
2022-06-29 19:09:22 -0400

    move data source location

---
+++ precache-manifest.7aee84ffa03a546da755f7ae2b31055e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a62ce7be96ef64fb8973",
+    "url": "/black-market-jesus/static/css/main.fb422e56.chunk.css"
+  },
+  {
+    "revision": "a62ce7be96ef64fb8973",
+    "url": "/black-market-jesus/static/js/main.a62ce7be.chunk.js"
+  },
+  {
+    "revision": "821a9a6ca9bf81106ef8",
+    "url": "/black-market-jesus/static/js/runtime~main.821a9a6c.js"
+  },
+  {
+    "revision": "bb7da18354fe9c465c38",
+    "url": "/black-market-jesus/static/js/2.bb7da183.chunk.js"
+  },
+  {
+    "revision": "234c5b09c0e58057d5a6fa39dc234bbd",
+    "url": "/black-market-jesus/static/media/people.234c5b09.csv"
+  },
+  {
+    "revision": "d947a55854f47954e9123fdf2016fddd",
+    "url": "/black-market-jesus/static/media/letters.d947a558.csv"
+  },
+  {
+    "revision": "de90af93cd8fe7fc14f71a6dae604674",
+    "url": "/black-market-jesus/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/black-market-jesus/precache-manifest.04c23ed273ebc891b25ebccec124ca7e.js"
+  "/black-market-jesus/precache-manifest.7aee84ffa03a546da755f7ae2b31055e.js"
 );
 
 workbox.clientsClaim();

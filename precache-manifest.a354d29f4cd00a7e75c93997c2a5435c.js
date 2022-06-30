5e3dc51fc7dd06d09c7a1a1f457e54d3507224ed
John Bellizzi <jabellizzi406@gmail.com>
2022-06-29 21:12:08 -0400

    removing subpage

---
+++ precache-manifest.a354d29f4cd00a7e75c93997c2a5435c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2159213ddc8b245edeb9",
+    "url": "/static/css/main.fb422e56.chunk.css"
+  },
+  {
+    "revision": "2159213ddc8b245edeb9",
+    "url": "/static/js/main.2159213d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6a48d22efd4e802f2ffd",
+    "url": "/static/js/2.6a48d22e.chunk.js"
+  },
+  {
+    "revision": "234c5b09c0e58057d5a6fa39dc234bbd",
+    "url": "/static/media/people.234c5b09.csv"
+  },
+  {
+    "revision": "d947a55854f47954e9123fdf2016fddd",
+    "url": "/static/media/letters.d947a558.csv"
+  },
+  {
+    "revision": "aa02d9c9a3f286de87ef70f5eaa74848",
+    "url": "/index.html"
+  }
+];
